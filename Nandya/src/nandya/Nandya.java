84ea82607f284dbddf1f53585aab291db3b1733b
Nandyaka <43085696+Nandyaka@users.noreply.github.com>
2018-09-09 11:36:07 +0700

    Add files via upload

---
+++ Nandya/src/nandya/Nandya.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nandya;
+
+/**
+ *
+ * @author Microsoft
+ */
+public class Nandya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TUGAS 
+        int n = 4;
+        for ( int i=0;i<n;i++){
+         for (int x=0;x<=i;x++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
